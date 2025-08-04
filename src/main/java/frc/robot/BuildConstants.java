48324702aabfecd2e64e2be14d690d0c8b42b0d0
ElliotScher <ecscher84@gmail.com>
2025-08-04 10:54:50 -0400

    no 2fa

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2k25_SystemCoreAlpha";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2025-07-25 23:12:59 EDT";
-  public static final long BUILD_UNIX_TIME = 1753499579662L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "aa4ac08948cf56203e027ff12c96095b1062299d";
+  public static final String GIT_DATE = "2025-07-25 23:13:53 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-08-04 10:54:00 EDT";
+  public static final long BUILD_UNIX_TIME = 1754319240514L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
